bf25eaf3b5bf8abb977dab7ed5e09efce7be1c55
Philst4 <pzweckbronner@gmail.com>
2025-05-30 15:34:13 -0400

    added processing script

---
+++ scripts/process_data.py
@@ -0,0 +1,56 @@
+# STL imports
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+# External imports
+import yaml
+import pandas as pd
+
+# Internal imports
+from src.data_processing import (
+    combine_train_test,
+    process_data,
+    merge_all
+)
+
+def load_config():
+    with open("config.yaml", "r") as f:
+        return yaml.safe_load(f)
+
+def load_raw_data(raw_path):
+    train = pd.read_csv(os.path.join(raw_path, "train.csv"))
+    test = pd.read_csv(os.path.join(raw_path, "test.csv"))
+    stores = pd.read_csv(os.path.join(raw_path, "stores.csv"))
+    oil = pd.read_csv(os.path.join(raw_path, "oil.csv"))
+    holidays = pd.read_csv(os.path.join(raw_path, "holidays_events.csv"))
+    return train, test, stores, oil, holidays
+
+def run_pipeline():
+    config = load_config()
+    RAW_DATA_PATH = config['data_paths']['raw']
+    CLEAN_DATA_PATH = config['data_paths']['clean']
+    
+    train, test, stores, oil, holidays = load_raw_data(RAW_DATA_PATH)
+    
+    main, train_ids, test_ids = combine_train_test(train, test)
+    main, stores, oil, holidays_events = process_data(
+        main,
+        stores, 
+        oil, 
+        holidays_events
+    )
+    merged = merge_all(main, stores, oil, holidays)
+
+    # Add 'is_train', 'is_test'
+    merged['is_train'] = merged['id'].isin(train_ids)
+    merged['is_test'] = merged['id'].isin(test_ids)
+
+    if not os.path.exists(CLEAN_DATA_PATH):
+        os.makedirs(CLEAN_DATA_PATH)
+
+    merged.to_csv(os.path.join(CLEAN_DATA_PATH, "clean.csv"), index=False)
+    print("Processing complete.")
+
+if __name__ == "__main__":
+    run_pipeline()
---
+++ src/data_processing.py
@@ -0,0 +1,165 @@
+# Internal imports
+
+
+# External imports
+import pandas as pd
+
+
+# Just basic combination of train + test
+def combine_train_test(train, test):
+    # Keep track of train + test ids
+    train_ids = list(train.id)
+    test_ids = list(test.id)
+
+    # Concat train + test -> main
+    main = pd.concat((train.copy(), test.copy()), axis=0)
+    return main, train_ids, test_ids
+
+#### CLEANING PROCESSES ####
+
+def clean_main(main):
+    # Make store_nbr, family into categorical
+    cat_features = ['store_nbr', 'family']
+    main[cat_features] = main[cat_features].astype('category')
+
+    # Convert date
+    main['date'] = pd.to_datetime(main['date'], format="%Y-%m-%d")
+    return main
+
+def clean_stores(stores):
+    stores = stores.copy()
+    cat_cols = ['store_nbr', 'city', 'state', 'type', 'cluster']
+    stores[cat_cols] = stores[cat_cols].astype('category')
+    return stores
+
+def clean_oil(oil):
+    oil = oil.copy()
+    oil.date = pd.to_datetime(oil.date, format="%Y-%m-%d")
+
+    # Fill in missing dates + add day of the week
+    date_range = pd.date_range(start=oil.date.min(), end=oil.date.max(), freq='D')
+    dates = pd.DataFrame({'date' : date_range})
+    oil = pd.merge(
+        oil, 
+        dates,
+        on='date',
+        how='right'
+    )
+
+    # Fill in missing oil values
+    oil = oil.sort_values(by=['date'])
+    oil['dcoilwtico'] = oil['dcoilwtico'].ffill()
+    
+    # Just fill in the first N/A value with the second value
+    oil['dcoilwtico'] = oil['dcoilwtico'].bfill()
+    return oil
+
+def clean_holidays_events(holidays_events):
+    holidays_events = holidays_events.copy()
+    
+    # Deal with transfers? 
+    # Every type 'transfer' corresponds to type 'holiday'
+    # Will keep the uncelebrated transfer == True rows
+    # Will change the celebrated type == 'transfer' rows to holiday type
+    holidays_events.loc[holidays_events['type'] == 'Transfer', 'type'] = 'Holiday'
+
+    # Convert categorical features
+    cat_features = ['type', 'locale', 'locale_name', 'description']
+    holidays_events[cat_features] = holidays_events[cat_features].astype('category')
+
+    # Convert date
+    holidays_events['date'] = pd.to_datetime(holidays_events['date'], format="%Y-%m-%d")
+    return holidays_events
+
+#### FEATURE ENGINEERING PROCESSES ####
+
+def fe_main(main):
+    # Idea for future: do stuff with onpromotion (groupby's?)
+    return main
+
+def fe_holidays_events(holidays_events):
+    """
+    Feature engineering process for holidays_events data.
+    
+    Main process is creating OHE's for combos of 'locale', 'locale_name', 'type'
+    """
+    
+    holidays_events = holidays_events.copy()
+    holidays_events['combo'] = list(
+        zip(
+            holidays_events['locale'], 
+            holidays_events['locale_name'], 
+            holidays_events['type']
+        )
+    )
+    ohes = pd.get_dummies(holidays_events['combo'], prefix='combo').astype(int)
+    holidays_events = pd.concat([holidays_events['date'], ohes], axis=1)
+    holidays_events = holidays_events.groupby('date').sum().reset_index()
+    holidays_events['is_holiday_event'] = 1
+    return holidays_events
+
+def fe_stores(stores):
+    return stores
+
+#### Main data-processing function
+def process_data(main, stores, oil, holidays_events):
+    main = fe_main(clean_main(main))
+    stores = fe_stores(clean_stores(stores))
+    oil = fe_stores(clean_stores(stores))
+    holidays_events = fe_holidays_events(
+        clean_holidays_events(
+            holidays_events
+            )
+    )
+
+    return main, stores, oil, holidays_events
+
+#### DATA-MERGING LOGIC ####
+def fe_merge2(merge2, cols):
+    """
+    Some more feature-engineering on the merged dataframe.
+    
+    merge2 is merge between oil & holidays_events.
+    """
+    merge2 = merge2.copy()
+    
+    # Add info about previous/next few days being a holiday
+
+    # Step 2: Create all shifted DataFrames
+    shifted_1next = merge2[cols].shift(-1).fillna(0).astype(int)
+    shifted_1next.columns = [col + '_1next' for col in cols]
+
+    shifted_2next = merge2[cols].shift(-2).fillna(0).astype(int)
+    shifted_2next.columns = [col + '_2next' for col in cols]
+
+    shifted_1before = merge2[cols].shift(1).fillna(0).astype(int)
+    shifted_1before.columns = [col + '_1before' for col in cols]
+
+    shifted_2before = merge2[cols].shift(2).fillna(0).astype(int)
+    shifted_2before.columns = [col + '_2before' for col in cols]
+
+    merge2 = pd.concat([merge2, shifted_1next, shifted_2next, shifted_1before, shifted_2before], axis=1)
+    return merge2
+
+def merge_all(main, stores, oil, holidays_events):
+    """
+    Intended to be ran after processing the raw dataframes.
+    """
+    
+    # Merge main with stores on 'store_nbr' -> merge1
+    merge1 = pd.merge(main, stores, on='store_nbr', how='left')
+
+    # Merge holiday events with oil on 'date' -> merge2
+    merge2 = pd.merge(oil, holidays_events, on='date', how='left')
+
+    # Fill N/A's in merge2
+    merge2[holidays_events.columns] = merge2[holidays_events.columns].fillna(0)
+
+    # Add info about previous/next few days being a holiday
+    cols = list(holidays_events.drop(columns='date').columns) # Define columns to shift
+    merge2 = fe_merge2(merge2, cols)
+
+    # Merge merge1 with merge2 on 'date' -> merge3
+    merge3 = pd.merge(merge1, merge2, on='date', how='left')
+    return merge3
+
